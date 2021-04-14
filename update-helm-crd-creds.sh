fd083b697a3e6f94c997ffbf5f0dbf176f8610f5
Jeremy McGee <jeremy.mcgee@nationwide.co.uk>
2021-04-14 21:56:42 +0100

    Updates creds

--- update-helm-crd-creds.sh
+++ update-helm-crd-creds.sh
@@ -4,6 +4,7 @@ helm repo add fluxcd https://charts.fluxcd.io
 helm upgrade -i helm-operator fluxcd/helm-operator \
     --namespace flux-system \
     --set configureRepositories.enable=true \
+    --set helm.versions=v3 \
     --set 'configureRepositories.repositories[0].name=example' \
     --set 'configureRepositories.repositories[0].url=https://familymcgee.jfrog.com/fls-docker-repo' \
     --set 'configureRepositories.repositories[0].username=fls-user' \
